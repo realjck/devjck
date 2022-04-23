1c0f6390dec9db1703acc253289d9ddb88002d1f
realjck <jckleinbourg@gmail.com>
2022-04-23 21:27:22 +0200

    Update April 2022

--- assets/app/app.js
+++ assets/app/app.js
@@ -9,19 +9,16 @@ $("#bt-back").on("click", function(){
 function showContact(shows){
 	if (shows){
 		$("#main-buttons").hide();
-		$("#contact-frame").fadeIn();
-		$("#secondary-buttons").fadeIn();
+		$("#contact-buttons").fadeIn();
 	} else {
-		$("#main-buttons").show();
-		$("#contact-frame").hide();
-		$("#secondary-buttons").hide();
+		$("#main-buttons").fadeIn();
+		$("#contact-buttons").hide();
 	}
 }
 
 
 // preload
 preloadXHR([
-    'assets/images/html/jck-portrait.jpg',
     'assets/libs/fontawesome/webfonts/fa-solid-900.woff2',
     'assets/libs/fontawesome/webfonts/fa-brands-400.woff2'
 ]);
