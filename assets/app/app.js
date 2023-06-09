254934e37d49b093b54248bed2a296760f819a13
realjck <jckleinbourg@gmail.com>
2023-06-09 21:29:25 +0200

    change resize to classic media query

--- assets/app/app.js
+++ assets/app/app.js
@@ -6,8 +6,6 @@ const wrapper = document.getElementById("wrapper");
 const front = document.getElementById("front");
 const back = document.getElementById("back");
 
-let wrapperScale = 1;
-
 rotateButtons.forEach(bt => bt.addEventListener('click', e => {
 
 	is_rotated = !is_rotated;
@@ -21,25 +19,9 @@ rotateButtons.forEach(bt => bt.addEventListener('click', e => {
 			front.style.display = "block";
 			back.style.display = "none";
 		}
-		wrapper.style.transform = "scaleX("+wrapperScale+")";
+		wrapper.style.transform = "scaleX(1)";
 	}, 350);
 }));
-
-// scale wrapper < 22em
-function resizeWrapper() {
-  if (window.innerWidth < 22 * parseFloat(getComputedStyle(document.documentElement).fontSize)) {
-    wrapperScale = window.innerWidth / (22 * parseFloat(getComputedStyle(document.documentElement).fontSize));
-    wrapper.style.transform = 'scale(' + wrapperScale + ')';
-  } else {
-    wrapper.style.transform = 'scale(1)';
-  }
-}
-
-// Call the function on page initialization
-window.addEventListener('DOMContentLoaded', resizeWrapper);
-
-// Call the function on window resize
-window.addEventListener('resize', resizeWrapper);
 
 
 console.log("🍔🌭🍔🌭🍔🌭🌭🌭 🍔🌭🍔🍔🍔🌭🍔🌭 🍔🍔🌭🍔🍔🍔🍔🍔 🍔🌭🍔🍔🍔🍔🍔🌭 🍔🌭🍔🍔🌭🌭🍔🍔 🍔🌭🍔🍔🌭🌭🍔🍔 🍔🍔🌭🍔🍔🍔🍔🍔 🍔🌭🍔🍔🌭🌭🍔🍔 🍔🌭🍔🍔🌭🍔🍔🌭 🍔🌭🍔🌭🍔🌭🌭🍔 🍔🌭🍔🍔🍔🌭🍔🌭 🍔🍔🌭🍔🍔🍔🍔🍔 🍔🌭🍔🍔🌭🍔🍔🌭 🍔🌭🍔🍔🌭🌭🌭🍔 🍔🍔🌭🍔🍔🍔🍔🍔 🍔🌭🍔🍔🍔🍔🍔🌭 🍔🍔🌭🍔🍔🍔🍔🍔 🍔🌭🍔🌭🌭🍔🍔🌭 🍔🌭🍔🍔🍔🌭🍔🌭 🍔🌭🍔🍔🌭🌭🍔🍔 🍔🌭🍔🍔🌭🌭🍔🍔 🍔🌭🍔🍔🌭🌭🌭🌭 🍔🌭🍔🌭🍔🌭🌭🌭 🍔🍔🌭🍔🍔🍔🍔🍔 🍔🌭🍔🌭🍔🍔🌭🌭 🍔🌭🍔🌭🍔🌭🍔🌭 🍔🌭🍔🍔🍔🍔🌭🍔 🍔🌭🍔🍔🌭🌭🍔🌭 🍔🌭🍔🍔🍔🍔🍔🌭 🍔🌭🍔🌭🍔🍔🌭🍔 🍔🌭🍔🍔🌭🍔🍔🌭 🍔🌭🍔🍔🌭🌭🌭🍔 🍔🌭🍔🍔🍔🌭🍔🌭");//why?
